90f57408e109287940240f6a275b0557e1c171a6
Sabrine Ziyani <sabrine.ziyani@student.ehb.be>
2025-02-27 13:09:49 +0100

    main

--- js/data.js
+++ js/data.js
@@ -1,5 +1,7 @@
 export function getAdjectives() {
     return JSON.stringify(
+
+
         [{
             "word": "nice",
             "score": 6.4,
--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ let adjectives;
 let sortDirection = "up";
 
 function init() {
+    let getAdjectives = getAdjectivesJSON();
+    console.log(getAdjectives);
+
+    adjectives = JSON.parse(getAdjectives);
+    render();
+
+
+
 
 }
 
@@ -22,29 +30,50 @@ function sort() {
 }
 
 function render() {
+    let htmlString = '';
 
-}
+    adjectives.forEach {
+        function(adjectives) {
+            console.log(adjectives.word, adjectives.score);
+            htmlString += "
+                < div class="word-item" >
 
-function upVote(target) {
+                    <span class="word-score bad">4</span>
+<span>ok</span>
+<div class="vote-buttens">
+<button vatue ="ok" class="upvote-button" > i</buttons>
+<button value = "ok" class="downvate-button">f< </button>
 
-}
+</div>
+</div >
+
+        
 
 
-function downVote(target) {
-
-}
-
-function updateScore(word, scoreChange) {
-    const foundIndex = adjectives.findIndex(function (item, index) {
-        if (item.word == word) {
-            return true
         }
-    });
 
-    if (foundIndex != null) {
-        let newScore = adjectives[foundIndex]['score'] + scoreChange;
-        adjectives[foundIndex]['score'] = Math.round(newScore * 100) / 100;
     }
-}
 
-init();
+    function upVote(target) {
+
+    }
+
+
+    function downVote(target) {
+
+    }
+
+    function updateScore(word, scoreChange) {
+        const foundIndex = adjectives.findIndex(function (item, index) {
+            if (item.word == word) {
+                return true
+            }
+        });
+
+        if (foundIndex != null) {
+            let newScore = adjectives[foundIndex]['score'] + scoreChange;
+            adjectives[foundIndex]['score'] = Math.round(newScore * 100) / 100;
+        }
+    }
+
+    init();
